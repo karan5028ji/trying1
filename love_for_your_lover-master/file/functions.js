4e3708cd6931df55e7fb3cdae5cc6aa7295e993b
karan5028ji <83299258+karan5028ji@users.noreply.github.com>
2024-02-08 14:40:12 +0530

    done 1.1

---
+++ love_for_your_lover-master/file/functions.js
@@ -0,0 +1,77 @@
+/*
+ * http://love.hackerzhou.me
+ */
+
+// variables
+var $win = $(window);
+var clientWidth = $win.width();
+var clientHeight = $win.height();
+
+$(window).resize(function () {
+    var newWidth = $win.width();
+    var newHeight = $win.height();
+    if (newWidth != clientWidth && newHeight != clientHeight) {
+        location.replace(location);
+    }
+});
+
+(function ($) {
+    $.fn.typewriter = function () {
+        // Return a promise
+        return new Promise((resolve, reject) => {
+            this.each(function () {
+                var $ele = $(this), str = $ele.html(), progress = 0;
+                $ele.html('');
+                var timer = setInterval(function () {
+                    var current = str.substr(progress, 1);
+                    if (current == '<') {
+                        progress = str.indexOf('>', progress) + 1;
+                    } else {
+                        progress++;
+                    }
+                    $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+                    if (progress >= str.length) {
+                        clearInterval(timer);
+                        // Resolve the promise when the animation is completed
+                        resolve();
+                    }
+                }, 75);
+            });
+        });
+    };
+})(jQuery);
+
+
+runAsync().start(function () {
+    // This function will be called after all animations are completed
+
+    // Wait for the text animation to complete
+    textAnimate().start().then(function () {
+        // Once text animation is complete, show the button
+        document.getElementById("nextButton").style.display = "block";
+    });
+});
+
+
+function timeElapse(date) {
+    var current = Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    seconds = seconds % 3600;
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    seconds = seconds % 60;
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    var result = "Day <span class=\"digit\">" + days + "</span> <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
+    $("#clock").html(result);
+}
+
